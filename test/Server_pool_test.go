c65c215fd77bee97ac9b69f77f65b8b4e8ee5d64
solozyx <zhaoyingxin@88.com>
2021-04-15 10:42:04 +0800

    rule

--- test/Server_pool_test.go
+++ test/Server_pool_test.go
@@ -29,8 +29,8 @@ end
 `
 
 func SleepTime() {
-	//println("睡觉")
-	//time.Sleep(100 * time.Second )
+	println("睡觉")
+	//time.Sleep(100 * time.Millisecond )
 }
 
 func Test_rp1(t *testing.T) {
@@ -61,7 +61,6 @@ func Test_rp1(t *testing.T) {
 		for {
 			isExist := pool.IsExist([]string{"1"})
 			println(fmt.Sprintf("exist 1... %+v", isExist))
-
 
 			isExist = pool.IsExist([]string{"2"})
 			println(fmt.Sprintf("exist 2... %+v", isExist))
